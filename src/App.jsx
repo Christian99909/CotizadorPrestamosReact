f551d1a66ec2983bf7546cd3869bce29c74f091b
Christian99909 <cristian99909@gmail.com>
2026-05-24 14:51:39 -0600

    Agrega componente Button y funciones para incrementar y decrementar cantidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,74 @@
 import {useState} from 'react'
 import Header from "./components/Header" 
+import Button from "./components/Button" 
+import {formatearDinero} from './helpers'
 
 
 function App() {
 
   const [cantidad, setCantidad] = useState(10000);
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
+
   
   function handleChange(e){
 
-    console.log(Number(e.target.value));
+    setCantidad(+e.target.value);
 
   }
+
+  function handleClickDecremento(e){
+
+    if (cantidad > MIN) {
+      
+      const valor = cantidad - STEP;
+      setCantidad(valor)
+    }
+  }
+
+  function handleClickAumento(e){
+
+    if (cantidad < MAX) {
+      
+      const valor = cantidad + STEP;
+      setCantidad(valor)
+    }
+  }
+
   
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
 
        <Header />
 
+       <div className='flex justify-between my-6'>
+
+        <Button 
+          operador = '-' 
+          fn = {handleClickDecremento}
+        />
+        <Button 
+          operador = '+'
+          fn = {handleClickAumento}
+        />
+
+        
+
+       </div>
+
        <input 
           type='range'
           className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600" 
           onChange={handleChange}
+          min={MIN}
+          max={MAX}
+          step={STEP}
+          value={cantidad}
         />
         
+        <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>{formatearDinero(cantidad)}</p>
+
     </div>
   )
 }
